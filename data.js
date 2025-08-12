0d5167aab816d33452dea43f22d70295b016842a
Ashley Rebecca Goldstein <90653195+aivxx@users.noreply.github.com>
2025-08-13 04:28:38 +0900

    Update data.js
    Great lab, thank you!

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
 const postcards = [
   { sketch: 'https://editor.p5js.org/davepagurek/sketches/7YRHVfZRl', canUseInMedia: true },
+  { Ash: 'https://editor.p5js.org/aivxx/full/67GcoW8nR', canUseInMedia: true},
 ];
